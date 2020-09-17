0241b2b53c265ab2534129509a55b3c9926d47f6
Keiran King <me@keiranking.com>
2020-09-17 15:06:12 -0400

    Convert HTML to Markdown
    This commit allows files that have frontmatter followed by an HTML body
    to be converted to Markdown. Hyperlinks are converted to an endnote
    format. Articles remain human-readable even when linking to multiple
    primary sources.

--- markdownize.rb
+++ markdownize.rb
@@ -7,26 +7,18 @@ end
 
 require 'kramdown'
 
-def move_markdown_links_to_endnotes(text)
-  full_link_regex = /\[.+?\]\(\S+\)/
-  reference_regex = /\[.+?\]/
-
-  in_place_links = text.scan(full_link_regex)
-  endnote_links = in_place_links.map { |link|
-    link.sub('](', ']: ').delete_suffix(')')
-  }
-
-  in_place_links.each do |link|
-    text.sub!(link, link[reference_regex])
-  end
-
-  text + "\n" + "\n" + endnote_links.join("\n")
-end
-
 filename = ARGV.first
 text = File.read(filename)
-# File.write(filename, move_markdown_links_to_endnotes(text))
-text.gsub!(/\n\n/, "<!--p-->")
-text.gsub!(/\n/, "<!--br-->")
-puts Kramdown::Document.new(text, input: 'html').to_kramdown
-puts "Mischief managed."
+
+frontmatter_regex = /---[\S\s]*?---/
+frontmatter = text.scan(frontmatter_regex).first
+body = text.sub(frontmatter, "")
+body.gsub!("<!--more-->", "")
+
+paragraph_marker = "<!--p-->"
+body.gsub!(/\n\n/, paragraph_marker)
+markdown_body = Kramdown::Document.new(body, input: 'html').to_kramdown
+markdown_body.gsub!(paragraph_marker, "")
+
+File.write(filename, frontmatter + markdown_body)
+puts filename + " converted to Markdown."
